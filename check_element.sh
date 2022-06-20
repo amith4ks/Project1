14d55267ac9a7032d910543d79b9820d5c11e9f8
Ubuntu <ubuntu@ip-172-31-90-50.ec2.internal>
2022-06-20 10:47:34 +0000

    added new script files

---
+++ check_element.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+name="Amith Girish Abhi Ramesh"
+read uname
+for i in $name
+do
+	if [ $i == $uname ]
+	then 
+		check=` echo "Yes"`
+	fi
+done
+if [ $check == "Yes" ]
+then
+	echo "$uname is found in array"	
+else
+echo "$uname is not found in array" 
+fi
